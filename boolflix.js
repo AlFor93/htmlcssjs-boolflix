5db0ffb3f78d0fde14e4679346b4bddbc6ce9f7c
AlFor93 <alby.fornasier93@gmail.com>
2019-03-27 01:17:26 +0100

    Aggiunta delle bandiere

--- boolflix.js
+++ boolflix.js
@@ -1,10 +1,12 @@
+//FUNZIONE PER LA CREAZIONE DEL TEMPLATE RICEVENDO TUTTI I PARAMETRI ESTERNI DALLA CHIAMATA AJAX
 function addDataFilmsHB (title,originalTitle,lenguage,vote) {
   var data = {
     title : title,
     originalTitle : originalTitle,
     lenguage : lenguage,
-    vote: vote,
-    stars: addStars(vote),
+    flag : addFlags (lenguage),
+    vote : vote,
+    stars : addStars(vote),
   };
 
   var template = $("#film-template").html();
@@ -14,21 +16,66 @@ function addDataFilmsHB (title,originalTitle,lenguage,vote) {
   var filmsCont = $(".film-container")
   filmsCont.append(finalMessageFormHtml);
 }
-
+//FUNZIONE PER CONVERTIRE IL VOTO (ARROTONDATO all'intero più vicino) IN STELLE
 function addStars (vote) {
   var starVote = Math.round(vote)/2;
-  var emptyStar = "<i class='far fa-star'></i>"
-  var spottyStar = "<i class='fas fa-star'></i>"
+  var emptyStar = "<i class='fas fa-star'></i>"
+  var spottyStar = "<i class='far fa-star'></i>"
+  var starsList = $(".starsList");
+  var newStar ="";
   for (var i = 0; i < 5; i++) {
     if (starVote <= i) {
-      var newStar = spottyStar;
+      newStar += spottyStar;
     } else {
-      var newStar = emptyStar;
+      newStar += emptyStar;
     }
   }
   return newStar;
 }
+//FUNZIONE PER L'AGGIUNTA DELLE BANDIERE
+function addFlags (lenguage) {
+  var flag = document.createElement("img");
+  switch (lenguage) {
+    case "fr":
+      flag.src = "flags_img/Flag_of_France.svg";
+      break;
+    case "it":
+      flag.src = "flags_img/Flag_of_Italy.svg";
+      break;
+    case "en":
+      flag.src = "flags_img/Flag_of_the_United_Kingdom.svg";
+      break;
+    case "de":
+      flag.src = "flags_img/Flag_of_Germany.svg";
+      break;
+    case "es":
+      flag.src = "flags_img/Flag_of_Spain.svg";
+      break;
 
+    default: flag.src = "flags_img/boh.png";
+  }
+
+  //Stessa cosa utilizzando una serie di if else concatenati
+  // if (lenguage == "fr") {
+  //   flag.src = "flags_img/Flag_of_France.svg";
+  // } else if (lenguage == "it") {
+  //   flag.src = "flags_img/Flag_of_Italy.svg";
+  // } else if (lenguage == "en") {
+  //   flag.src = "flags_img/Flag_of_the_United_Kingdom.svg";
+  // } else if (lenguage == "de") {
+  //   flag.src = "flags_img/Flag_of_Germany.svg";
+  // } else {
+  //   flag.src = "flags_img/boh.png";
+  // }
+  flag.className = "flagSize";
+  return flag.outerHTML;
+}
+//FUNZIONE PER RIPULIRE I VECCHI RISULTATI
+function clearOldData () {
+  var film = $(".film");
+  film.remove();
+}
+//FUNZIONE PER RICERCARE I VALORI INTERESSATI ALL'INTERNO DELL'ARRAY CHE VIENE RESTITUITO DALLA CHIAMATA AJAX
 function ajaxSearch (data) {
   var res = data.results;
   for (var i = 0; i < res.length; i++) {
@@ -36,28 +83,26 @@ function ajaxSearch (data) {
     var originalTitle = res[i].original_title;
     var lenguage = res[i].original_language;
     var vote = res[i].vote_average;
-    // addStars (vote);
     addDataFilmsHB (title,originalTitle,lenguage,vote);
   }
 }
-
-function ajaxCall (title) {
+//FUNZIONE PER RICHIAMARE I DATI DI TUTTI I FILM CON UN DATO TITOLO, TRAMITE UN API
+function ajaxCall () {
+  var search = $("#searchBar").val();
   $.ajax({
     url: "https://api.themoviedb.org/3/search/movie",
     method: "GET",
     data: {
       api_key: "904095b7e749c808eeb90f272724f68e",
       language: "it-IT",
-      query: title,
+      query: search,
+      // page :
     },
     success: function (inData){
-      var pages = inData.total_pages;
-      console.log("total pages: " + pages);
-      for (var k = 1; k <= pages; k++) {
-        var page = k;
-        console.log("page nr: " + page);
-        ajaxSearch(inData);
-      }
+      // var pages = inData.total_pages;
+      // console.log("total pages: " + pages);
+      clearOldData();
+      ajaxSearch(inData);
     },
     error: function (request, state, error){
       console.log("request",request);
@@ -84,11 +129,13 @@ function ajaxCall (title) {
 
 function init() {
   var searchButton = $("#my-btn");
-  searchButton.click(function(){
-    var search = $("#searchBar").val();
-    ajaxCall(search);
+  searchButton.click(ajaxCall);
+  var search = $("#searchBar");
+  search.on("keyup",function(){
+    if (event.which==13) {
+      ajaxCall();
+    }
   });
-  // var search = $("#searchBar").val();
 }
 
 $(document).ready(init);
